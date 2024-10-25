4ed10b8bef3dfb69adb0956504c5b5a8673ea552
Theo3225 <tchartie@ucsc.edu>
2024-10-24 21:13:17 -0700

    Session 1 prompt

---
+++ session1.py
@@ -0,0 +1,9 @@
+def Session1():
+	print("My name is Theo Chartier. My favorite movie is Thelma and Louise. My favorite food is bibimbap because I love spice and crispy rice.")
+
+def main():
+	Session1()
+
+if __name__=="__main__":
+	main()
+
